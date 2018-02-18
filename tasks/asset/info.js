e24f3c2991007568045b70b74387857c7d60bfdf
Christian Bäuerlein <fabrik42@gmail.com>
2018-02-18 18:20:49 +0100

    Refactoring success callback

--- tasks/asset/info.js
+++ tasks/asset/info.js
@@ -1,29 +1,30 @@
 const Table = require('cli-table');
 const { server, asset } = require('../../config');
 
+const renderInfo = response => {
+  const info = response.records[0];
+  const table = new Table();
+
+  table.push(
+    { Code: info.asset_code },
+    { Type: info.asset_type },
+    { 'Total Amount': info.amount },
+    { 'Num Accounts': info.num_accounts },
+    { 'Flag: Auth Required': info.flags.auth_required },
+    { 'Flag: Auth Revocable.': info.flags.auth_revocable },
+    { 'TOML Link': info._links.toml.href }
+  );
+
+  console.log(table.toString());
+};
+
 server
   .assets()
   .forIssuer(asset.getIssuer())
   .forCode(asset.getCode())
   .limit(1)
   .call()
-  .then(response => {
-    const info = response.records[0];
-
-    const table = new Table();
-
-    table.push(
-      { Code: info.asset_code },
-      { Type: info.asset_type },
-      { 'Total Amount': info.amount },
-      { 'Num Accounts': info.num_accounts },
-      { 'Flag: Auth Required': info.flags.auth_required },
-      { 'Flag: Auth Revocable.': info.flags.auth_revocable },
-      { 'TOML Link': info._links.toml.href }
-    );
-
-    console.log(table.toString());
-  })
+  .then(renderInfo)
   .catch(e => {
     console.error('Error!');
     console.dir(e);
